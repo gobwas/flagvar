d68e800eee056fcbec2bc72ce646a3b12605aed2
Sergey Kamardin <gobwas@gmail.com>
2021-07-11 16:18:02 +0300

    strings: add separator

--- strings.go
+++ strings.go
@@ -1,22 +1,33 @@
 package flagvar
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type StringsValue struct {
-	P *[]string
+	P         *[]string
+	Separator string
 }
 
-func Strings(p *[]string) *StringsValue {
+func Strings(p *[]string, sep string) *StringsValue {
 	return &StringsValue{
-		P: p,
+		P:         p,
+		Separator: sep,
 	}
 }
 
 func (s *StringsValue) Set(v string) error {
-	(*s.P) = append((*s.P), v)
+	(*s.P) = append((*s.P), strings.Split(v, s.Separator)...)
 	return nil
 }
 
 func (s *StringsValue) String() string {
+	if s.P == nil {
+		return ""
+	}
+	if sep := s.Separator; sep != "" {
+		return strings.Join((*s.P), sep)
+	}
 	return fmt.Sprintf("%s", (*s.P))
 }
